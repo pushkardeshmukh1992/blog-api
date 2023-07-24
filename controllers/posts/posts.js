be550cef7efdead37fdfe7e7c8c8317876232b6d
Pushkar Deshmukh <pushkar2192@gmail.com>
2023-07-24 13:26:56 +0530

    Complete CRUD operation for post

--- controllers/posts/posts.js
+++ controllers/posts/posts.js
@@ -2,6 +2,7 @@ const asyncHandler = require("express-async-handler");
 const Post = require("../../model/Post/Post");
 const User = require("../../model/User/User");
 const Category = require("../../model/Category/Category");
+const postsRouter = require("../../routes/post/postRouter");
 
 //@desc Create post
 //@route POST /api/v1/posts
@@ -47,6 +48,64 @@ exports.createPost = asyncHandler(async (req, res) => {
   res.json({
     status: "success",
     message: "Post successfully created",
+    post,
+  });
+});
+
+//@desc Get all posts
+//@route GET /api/v1/posts
+//@access public
+
+exports.getPosts = asyncHandler(async (req, res) => {
+  const posts = await Post.find({});
+
+  res.status(201).json({
+    status: "success",
+    message: "Posts successfully created",
+    posts,
+  });
+});
+
+//@desc Get single post
+//@route GET /api/v1/post/:id
+//@access public
+
+exports.getPost = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  res.status(201).json({
+    status: "success",
+    message: "Posts successfully created",
+    post,
+  });
+});
+
+//@desc Delete post
+//@route DELETE /api/v1/posts/:id
+//@access private
+
+exports.deletePost = asyncHandler(async (req, res) => {
+  await Post.findByIdAndDelete(req.params.id);
+
+  res.status(201).json({
+    status: "success",
+    message: "Posts successfully deleted",
+  });
+});
+
+//@desc Update post
+//@route PUT /api/v1/posts/:id
+//@access private
+
+exports.updatePost = asyncHandler(async (req, res) => {
+  const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(201).json({
+    status: "success",
+    message: "Post successfully updated",
     post,
   });
 });
